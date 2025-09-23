5df2afa874841083cdff8b8e93a152a01956d98f
s1411222041 <chenlele.alex@gmail.com>
2025-09-23 16:13:23 +0800

    0923

--- bmi.js
+++ bmi.js
@@ -6,5 +6,16 @@ var height=readline.
 questionInt("enter your height in cm:");
 var bmi=weight/((height/100)**2)
 
+function calculateBMI(weight, height) {
+    if (height<50 || height>300) {
+        console.log("height should be between 50cm to 300cm");
+        return;
+    }
+    if (weight<10 || weight>500) {
+        console.log("weight should be between 10kg to 500kg");
+        return;
+    }
+let bmi=weight/((height/100)**2)
 
 console.log("hellow your bmi is:" + bmi);
+}
