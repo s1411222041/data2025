58bd7a83260db3664c8a16220762d112ddb7925d
s1411222041 <chenlele.alex@gmail.com>
2025-09-30 16:07:44 +0800

    0930

---
+++ sum.js
@@ -0,0 +1,11 @@
+function sum(n){
+    var result=0;
+    var i=0;
+    while(i++<=n){
+        result += i;
+        
+    }
+    return result;
+}
+var n=10;
+console.log("1+2+...+"+n+"="+sum(10));
