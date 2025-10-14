bdd5ec0a7c791472a0fb2bc4fc47aa2ee74af3c6
s1411222041 <chenlele.alex@gmail.com>
2025-10-14 13:57:51 +0800

    1014

--- avgWeighted.js
+++ avgWeighted.js
@@ -8,17 +8,20 @@
 //     return average;//1
 // }
 
-function avg(score){
+function avgWeight(score, weight){
     var sum=0;//1
+    var sumWeight=0;
     for(var i=0;i<score.length;i++){//n+1
-        sum += score[i];  //n
+        sum += score[i]*weight[i];  //n
+        sumWeight += weight[i];
     }
-    return (score.length>0) ? sum/score.length : 0;
+    return (score.length>0) ? sum/sumWeight : 0;
 }
 
 //Test
 //var Score=[50,20,30,15,16,21,33,11,-9,27];
 var Score=[];
+var Weight=[6,5,5,3,3]
 const num=5;
 //input
 var readline=require("readline-sync");
@@ -26,5 +29,5 @@ for (let i = 0; i < num; i++) {
     Score[i]=readline.questionInt("Input "+(i+1)+"'s score?");
     
 }
-console.log(Score.toString()+" average:"+ avg(Score))
+console.log(Score.toString()+" average:"+ avgWeight(Score,Weight))
 
